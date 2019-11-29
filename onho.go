db92c3a8c102940ee4b018c10481169e094b9f8e
Michal K <kuritka@gmail.com>
2019-11-29 16:24:30 +0100

    fixing dockers

--- onho.go
+++ onho.go
@@ -3,24 +3,11 @@ package main
 import (
 	"github.com/kuritka/onho.io/cmd"
 	"github.com/kuritka/onho.io/common/log"
-	"os/user"
 )
 
 var logger   = log.Log
 
 func main() {
-
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
-
-	// Current User
-	logger.Info().Msg("Hi " + user.Name + " (id: " + user.Uid + ")")
-	logger.Info().Msg("Username: " + user.Username)
-	logger.Info().Msg("Home Dir: " + user.HomeDir)
-
-
 	cmd.Execute()
 }
 
