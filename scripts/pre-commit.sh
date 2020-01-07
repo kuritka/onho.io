4127b2b5473f8e95dd012d3c8d8414dbd8bf797f
Michal K <kuritka@gmail.com>
2020-01-07 14:37:54 +0100

    fix hook

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -4,3 +4,5 @@
 
 git diff --cached --name-only | grep -E '*.yaml' |   GREP_COLOR='4;5;30;33' xargs grep --color --with-filename -n -i 'kind: Secret' && echo 'COMMIT REJECTED. Please encrypt before commiting' && exit 1
 
+exit 0
+
